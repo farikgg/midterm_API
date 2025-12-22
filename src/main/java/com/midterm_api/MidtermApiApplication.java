f5ab000909ec14b94582143e4544be08b96c01c1
farikgg <farikfarik319@gmail.com>
2025-12-22 21:31:08 +0500

    Initial commit

---
+++ src/main/java/com/midterm_api/MidtermApiApplication.java
@@ -0,0 +1,13 @@
+package com.midterm_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidtermApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidtermApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/midterm_api/MidtermApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midterm_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidtermApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
